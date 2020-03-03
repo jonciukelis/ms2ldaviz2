899c6687bcaa78937721db56ea7bb202d04b6298
Jonas <jonciukelis@gmail.com>
2020-03-03 08:50:39 +0000

    Added build to deploy

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-eval "virtualenv flask"
-eval ". flask/bin/activate"
-eval "pip3 install -r requirements"
-cd react
-eval "npm install"
-eval "npm run build"
---
+++ react/build/precache-manifest.aead358b053b0c31b5fc93358358c2dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8019db728acf2e04e6a2f9816ceb9ff5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a0c47fab445763744c7",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ef5033e9ae2766d603be",
+    "url": "./static/css/main.c67ef966.chunk.css"
+  },
+  {
+    "revision": "4a0c47fab445763744c7",
+    "url": "./static/js/2.2fa927ff.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "./static/js/2.2fa927ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5033e9ae2766d603be",
+    "url": "./static/js/main.3ffb2d17.chunk.js"
+  },
+  {
+    "revision": "e1ed0f3fc335f64e07bc",
+    "url": "./static/js/runtime-main.ea6428cf.js"
+  }
+]);
