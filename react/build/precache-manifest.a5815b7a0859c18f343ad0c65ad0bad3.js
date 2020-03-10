c8e094420c56f59b2438e81f71c350acf23bdff9
Jonas <jonciukelis@gmail.com>
2020-03-10 10:37:52 +0000

    update with build folder

---
+++ react/build/precache-manifest.a5815b7a0859c18f343ad0c65ad0bad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227d4a722b41b21edaf8ba1409cf10e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12bb2db0e4e56bf593ac",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1cc2d1fa2d6359c3216b",
+    "url": "./static/css/main.5b72eecb.chunk.css"
+  },
+  {
+    "revision": "12bb2db0e4e56bf593ac",
+    "url": "./static/js/2.cc15da50.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "./static/js/2.cc15da50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc2d1fa2d6359c3216b",
+    "url": "./static/js/main.6866dc12.chunk.js"
+  },
+  {
+    "revision": "64622f3ad5ca5f43c5b0",
+    "url": "./static/js/runtime-main.18861739.js"
+  }
+]);
